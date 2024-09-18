89a0d7510aba3cabedb7b1c5eaa1a0d0abcaee90
IEVN1003-20001716 <rlystudios21@gmail.com>
2024-09-17 19:37:03 -0600

    17 de septiembre del 2024 OcultarImagen

---
+++ basesAngular/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'basesAngular';
+
+  pelicula={
+    nombre: 'SuperMan',
+    fechadeLanzamineto:new Date() , 
+    precio: 123.65
+    
+  }
+
+  duplicarNumero(valor:number): number{
+    return valor*2; 
+  }
+
+}
+
